bb4f5ab132a5ac19eeb08c148ba7214aba27797b
GrandOichii <i.hawk@yandex.ru>
2023-03-27 11:38:18 +0400

    Started websocket testing

---
+++ front/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace Example
+{
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      using (var ws = new WebSocket ("ws://localhost:8080/Laputa")) {
+        ws.OnMessage += (sender, e) =>
+                          Console.WriteLine ("Laputa says: " + e.Data);
+
+        ws.Connect ();
+        ws.Send ("BALUS");
+        Console.ReadKey (true);
+      }
+    }
+  }
+}
---
+++ server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Example
+{
+  public class Laputa : WebSocketBehavior
+  {
+    protected override void OnMessage (MessageEventArgs e)
+    {
+      var msg = e.Data == "BALUS"
+                ? "Are you kidding?"
+                : "I'm not available now.";
+
+      Send (msg);
+    }
+  }
+
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      var wssv = new WebSocketServer ("ws://localhost:8080");
+
+      wssv.AddWebSocketService<Laputa> ("/Laputa");
+      wssv.Start();
+      System.Console.WriteLine("Server started");
+      Console.ReadKey(true);
+      wssv.Stop();
+    }
+  }
+}
