2795197083b6f3cec2a96864f216f49d0df9e446
Igor Yastrebov <i.hawk@yandex.ru>
2023-04-22 10:42:47 +0400

    Added spell with 2 keywords

---
+++ cards/collections/test_set/cards/Black_Market_Trade/script.lua
@@ -0,0 +1,11 @@
+
+
+function _CreateCard( props )
+    props.cost = 2
+    local result = CardCreation:Spell(props)
+
+    result:AddKeyword('virtuous')
+    result:AddKeyword('evil')
+    
+    return result
+end
